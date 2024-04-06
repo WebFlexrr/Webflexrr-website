aaf663099dbc677bdde40091657de231f2a6f2d1
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-04-07 00:04:43 +0530

    fix: new hero section

---
+++ src/components/ui/Hero/ServiceSectionItem.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import React, { type FC } from "react";
+import Image from "next/image";
+import { Card, CardFooter } from "@nextui-org/react";
+
+interface ServiceSectionProps {
+	name: string;
+	slug: string;
+	description: string;
+	image: string;
+}
+const ServiceSectionItem: FC<ServiceSectionProps> = ({
+	name,
+	slug,
+	description,
+	image,
+}) => {
+	return (
+		<Link href={`/services/${slug}`}>
+			<Card
+				isFooterBlurred
+				radius="lg"
+				className="w-full max-w-[22rem] border border-purple-600 "
+			>
+				{/* <CardBody className="overflow-visible py-2"> */}
+				<Image
+					className="rounded-xl object-cover"
+					src={image}
+					width={1080}
+					height={1080}
+					alt={name}
+				/>
+				{/* </CardBody> */}
+				<CardFooter className="flex-col items-start px-4 pb-0 pt-2">
+					<div className=" mx-auto flex h-fit w-full flex-col gap-5 text-center hover:text-primary md:gap-6 xl:px-11">
+						<Link
+							href={`/services/${slug}`}
+							className=" text-center text-[30px]  font-bold leading-[41.76px] text-secondary transition duration-500  ease-in-out group-hover:text-primary md:text-[30px]"
+						>
+							{name}
+						</Link>
+						<span className="">
+							<p>
+								{description.split(" ").splice(0, 20).join(" ").concat("....")}
+							</p>
+						</span>
+					</div>
+				</CardFooter>
+			</Card>
+		</Link>
+		// 	 <section className=" servicesSections  group mx-auto  mb-20 flex  w-full max-w-[20rem]  items-center gap-10 rounded-lg border border-black p-10 shadow-xl lg:gap-7 xl:px-0 ">
+		// 	 <section className="aspect-[4/3]  w-[15rem]">
+		// 	<Link href={`/services/${slug}`}>
+		// 	<Image
+		// 	src={image}
+		// 	width={'1080'}
+		// 	height={'1080'}
+		// 	alt={name}
+		// 	// className="xl:absolute w-full xl:scale-75 xl:opacity-0 group-hover:visible group-hover:scale-100 group-hover:opacity-100 top-0 bg-white transition ease-in-out duration-300 "
+		// 	className=" relative mx-auto  "
+		// 			/>
+		// 		</Link>
+		// 	</section>
+
+		// 	<div className=" mx-auto flex h-fit w-full flex-col gap-5 text-center hover:text-primary md:gap-6 xl:px-11">
+		// 		<Link
+		// 			href={`/services/${slug}`}
+		// 			className=" text-center text-[30px]  font-bold leading-[41.76px] text-secondary transition duration-500  ease-in-out group-hover:text-primary md:text-[30px]"
+		// 		>
+		// 			{name}
+		// 		</Link>
+		// 		<span className="">
+		// 			<p>{description.split(' ').splice(0, 20).join(' ').concat('....')}</p>
+		// 		</span>
+		// 	</div>
+		//  </section>
+	);
+};
+
+export default ServiceSectionItem;
---
+++ src/components/ui/Navbar/Sidebar.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React, { type FC } from "react";
+import { RxCross2 } from "react-icons/rx";
+
+interface SideBarProps {
+	hidden: boolean;
+	toggle: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const SideBar: FC<SideBarProps> = ({ hidden, toggle }) => {
+	return (
+		<aside
+			className={`fixed ${
+				!hidden ? " invisible translate-x-[15rem]" : " visible translate-x-0"
+			} z-50 flex h-full w-full justify-end bg-transparent transition-all duration-700 ease-in-out`}
+		>
+			<section className="h-full w-[15rem] bg-background2">
+				<nav className="h-fit w-full">
+					<ul className="flex h-auto w-full flex-col text-left text-base leading-[42px] tracking-[2.8px] ">
+						<li className="flex w-full justify-end border-b border-subHeading px-5 py-3 ">
+							<RxCross2
+								onClick={() => {
+									toggle(false);
+								}}
+								className="h-6 w-6 text-secondary"
+							/>
+						</li>
+						<li className="h-11 w-full border-b border-subHeading p-0 hover:bg-primary ">
+							<a
+								href={"/#home"}
+								onClick={() => {
+									toggle(false);
+								}}
+								className="flex h-full w-full justify-start px-5  text-secondary transition duration-500 ease-in-out"
+							>
+								Home
+							</a>
+						</li>
+						<li className="h-11 w-full border-b border-subHeading p-0 hover:bg-primary ">
+							<a
+								href={"/services"}
+								onClick={() => {
+									toggle(false);
+								}}
+								className="flex h-full w-full justify-start px-5  text-secondary transition duration-500 ease-in-out"
+							>
+								Services
+							</a>
+						</li>
+						<li className="h-11 w-full border-b border-subHeading p-0 hover:bg-primary ">
+							<a
+								href={"/portfolio"}
+								onClick={() => {
+									toggle(false);
+								}}
+								className="flex h-full w-full justify-start px-5  text-secondary transition duration-500 ease-in-out"
+							>
+								Portfolio
+							</a>
+						</li>
+						<li className="h-11 w-full border-b border-subHeading p-0 hover:bg-primary ">
+							<a
+								href={"/teams"}
+								onClick={() => {
+									toggle(false);
+								}}
+								className="flex h-full w-full justify-start px-5  text-secondary transition duration-500 ease-in-out"
+							>
+								Teams
+							</a>
+						</li>
+						{/* <li className="w-full h-11 p-0 border-b border-subHeading hover:bg-primary ">
+              <Link
+                href={"/#blog"}
+                onClick={() => toggle(false)}
+                className="w-full h-full px-5 flex justify-start  text-secondary transition ease-in-out duration-500"
+              >
+                Blogs
+              </Link>
+            </li> */}
+						<li className="h-11 w-full border-b border-subHeading p-0 hover:bg-primary ">
+							<a
+								href={"/plans"}
+								onClick={() => {
+									toggle(false);
+								}}
+								className="flex h-full w-full justify-start px-5  text-secondary transition duration-500 ease-in-out"
+							>
+								Plans
+							</a>
+						</li>
+						<li className="h-11 w-full border-b border-subHeading p-0 hover:bg-primary ">
+							<a
+								href={"/contact"}
+								onClick={() => {
+									toggle(false);
+								}}
+								className="flex h-full w-full justify-start px-5  text-secondary transition duration-500 ease-in-out"
+							>
+								Contact Us
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</section>
+		</aside>
+	);
+};
+
+export default SideBar;
