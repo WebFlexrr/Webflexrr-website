284bfe6f5429b2a4615e6f69ffd27a5a834eb0eb
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2025-01-28 00:08:18 +0530

    feat: add scroll animation in about section

--- src/app/(Home)/components/About.tsx
+++ src/app/(Home)/components/About.tsx
@@ -1,27 +1,32 @@
 "use client";
+import { TextReveal } from "@/components/ui/text-reveal";
 import React from "react";
 
 const About = () => {
 	return (
-		<section className="mx-auto w-full  max-w-7xl  px-6 pt-40 md:py-20 ">
-			<section className="flex w-full flex-col gap-4 text-6xl font-medium  text-white lg:text-7xl   ">
-				<div className="flex gap-2">
-					<span>We're a</span>
-					<span className="text-primary">full-service</span>
+		<section className="mx-auto mt-20 w-full  max-w-7xl px-4 sm:px-6">
+			<div className="z-10 flex min-h-64 items-center justify-center rounded-lg  bg-white p-4 dark:bg-black sm:p-6">
+				<TextReveal text="We're a AI based Software Development Agency. 💫 We turn business Into industry Leader" />
+			</div>
 
-					<span>AI based Software</span>
-				</div>
-				<div className="flex gap-2">
-					<span>Development Agency. 💫</span>
+			{/* <section className="flex w-full flex-col gap-4 text-6xl font-medium  text-white lg:text-7xl   ">
+                <div className="flex gap-2">
+                    <span>We're a</span>
+                    <span className="text-primary">full-service</span>
 
-					<span>We turn</span>
-				</div>
-				<span className="flex gap-2 ">
-					business
-					<span className="text-primary">AI-driven</span>
-					Into industry leaders.
-				</span>
-			</section>
+                    <span>AI based Software</span>
+                </div>
+                <div className="flex gap-2">
+                    <span>Development Agency. 💫</span>
+
+                    <span>We turn</span>
+                </div>
+                <span className="flex gap-2 ">
+                    business
+                    <span className="text-primary">AI-driven</span>
+                    Into industry leaders.
+                </span>
+            </section> */}
 		</section>
 	);
 };
--- src/app/(Home)/components/Hero.tsx
+++ src/app/(Home)/components/Hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
 				className="-top-40 left-0 md:-top-20 md:left-60"
 				fill="white"
 			/>
-			<section className="relative  flex h-fit w-full items-center justify-center bg-background   pt-48 lg:h-screen lg:pt-28 ">
+			<section className="relative  mb-20 flex h-auto w-full items-center justify-center bg-background   pt-48 lg:h-screen lg:pt-28 ">
 				<section className="mx-auto  flex h-fit w-full max-w-[90rem] flex-col items-start px-7 text-center md:items-center md:px-14 xl:px-20">
 					{/* <motion.span
 						initial={{ opacity: 0, scale: 0.7, y: "-70%" }}
